ddc5523cf3dc5be21d175e237e085535e46fa69b
QWERTYistoobasic <91909097+QWERTYistoobasic@users.noreply.github.com>
2022-08-17 00:47:54 +0530

    Time: 675 ms (5.02%), Space: 309.8 MB (5.02%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
      int f(int idx, int prev, string& s, vector<int>& time, vector<vector<int>>& dp) {
-        if(idx == 0) {
+        if(idx ==s.size()-1) {
             if(s[idx] - 'a' == prev) return time[idx];
             return 0;
         }
@@ -10,8 +10,8 @@ public:
         
         int remove = 1e8, notRemove = 1e8;
         
-        remove = time[idx] + f(idx - 1, prev, s, time, dp);
-        if(s[idx] - 'a' != prev) notRemove = f(idx - 1, s[idx] - 'a', s, time ,dp); 
+        remove = time[idx] + f(idx+1, prev, s, time, dp);
+        if(s[idx] - 'a' != prev) notRemove = f(idx+1, s[idx] - 'a', s, time ,dp); 
         
         return dp[idx][prev] = min(remove, notRemove);
     }
@@ -19,7 +19,7 @@ public:
         int n = colors.size();
         vector<vector<int>>dp(n+1, vector<int>(27, -1));
         
-        return f(n-1, 26, colors, time, dp);
+        return f(0, 26, colors, time, dp);
         
     }
 };
