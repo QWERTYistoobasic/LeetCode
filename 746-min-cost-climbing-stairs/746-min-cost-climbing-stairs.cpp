fa2f3387430261580d2d6af8d0ad53a8d409d224
QWERTYistoobasic <91909097+QWERTYistoobasic@users.noreply.github.com>
2022-08-22 06:07:50 +0530

    Time: 17 ms (17.98%), Space: 13.9 MB (9.68%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        cost.push_back(-1);
+        int n=cost.size();
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<n;i++)
+        {
+            int left=dp[i-1]+cost[i-1];
+            int right=dp[i-2]+cost[i-2];
+            dp[i]=min(left,right);
+        }
+        return dp[n-1];
+    }
+};
