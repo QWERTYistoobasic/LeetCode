76993f1616ebc0c12aa1deb9b135bb2e5475f98b
QWERTYistoobasic <91909097+QWERTYistoobasic@users.noreply.github.com>
2022-09-01 22:23:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.64%) - LeetHub

---
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string str=to_string(num);
+        int count=0;
+        for(int i=0;i<=str.size()-k;i++)
+        {
+            int x=stoi(str.substr(i,k));
+            if(x!=0&&num%x==0)
+            count++;
+        }
+        return count;
+    }
+};
