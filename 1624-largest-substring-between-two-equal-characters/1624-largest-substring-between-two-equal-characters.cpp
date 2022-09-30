71603cf777a2d539a60aa46d31dc06cf234d6e44
QWERTYistoobasic <91909097+QWERTYistoobasic@users.noreply.github.com>
2022-09-30 19:37:05 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (29.26%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        unordered_map<char,int> mp;
+        int res=-1;
+        for(int i=0;i<s.length();i++){
+            if(mp.find(s[i])!=mp.end()){
+                res=max(res,i-mp[s[i]]-1);
+            }
+           else
+               mp[s[i]]=i;
+        }
+        return res;
+    }
+};
